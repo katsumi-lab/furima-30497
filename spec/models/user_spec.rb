d85b0c5d2b69ef8d3e9813b430ba52977a263aed
katsumi-lab <71427533+katsumi-lab@users.noreply.github.com>
2020-10-15 19:22:00 +0900

    factory_botのインストール

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,44 @@
 require 'rails_helper'
-
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'ユーザー新規登録' do
+    it "nicknameが空だと登録できない" do
+      user = User.new(nickname: "", email: "kkk@gmail.com", password: "00000000", password_confirmation: "00000000")
+      user.valid?
+      expect(user.errors.full_messages).to include("Nickname can't be blank")
+    end
+    it "emailが空では登録できない" do
+      user = User.new(nickname: "abe", email: "", password: "00000000", password_confirmation: "00000000")
+      user.valid?
+      expect(user.errors.full_messages).to include("Email can't be blank")
+    end
+    it "passwordが空だと登録できない" do
+      user = User.new(nickname: "abe", email: "kkk@gmail.com", password: "")
+      user.valid?
+      expect(user.errors.full_messages).to include("Password can't be blank")
+    end
+    it "emailが空では登録できない" do
+      user = User.new(nickname: "abe", email: "", password: "00000000", password_confirmation: "00000000")
+      user.valid?
+      expect(user.errors.full_messages).to include("Email can't be blank")
+    end
+    it "family_nameが空だと登録できない" do
+      user = User.new(nickname: "", email: "kkk@gmail.com", password: "00000000", password_confirmation: "00000000")
+      user.valid?
+      expect(user.errors.full_messages).to include("Nickname can't be blank")
+    end
+    it "emailが空では登録できない" do
+      user = User.new(nickname: "abe", email: "", password: "00000000", password_confirmation: "00000000")
+      user.valid?
+      expect(user.errors.full_messages).to include("Email can't be blank")
+    end
+    it "passwordが空だと登録できない" do
+      user = User.new(nickname: "abe", email: "kkk@gmail.com", password: "")
+      user.valid?
+      expect(user.errors.full_messages).to include("Password can't be blank")
+    end
+    it "emailが空では登録できない" do
+      user = User.new(nickname: "abe", email: "", password: "00000000", password_confirmation: "00000000")
+      user.valid?
+      expect(user.errors.full_messages).to include("Email can't be blank")
+  end
 end
