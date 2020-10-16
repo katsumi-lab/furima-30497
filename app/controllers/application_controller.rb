5bf7ed30c8849e4616131f124a745505a52b7ad6
katsumi-lab <71427533+katsumi-lab@users.noreply.github.com>
2020-10-16 12:21:13 +0900

    新規登録画面のビューファイル

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,10 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
+  # before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
   def configure_permitted_parameters
+    binding.pry
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :family_name, :first_name, :family_name_kana, :first_name_kana, :date])
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,17 @@
 class UsersController < ApplicationController
-  def edit
-    
-  end
+ 
+#   before_action :authenticate_user!, only: [:new, :edit, :create]
+
+# def create
+#     @user = User.new(users_params)
+#     @user.save
+# end
+
+
+# private 
+#     def users_params
+#       params.require(:user).permit(:nickname , :family_name, :first_name, :family_name_kana, :first_name_kana, :date )
+#     end
+# end
+
 end
