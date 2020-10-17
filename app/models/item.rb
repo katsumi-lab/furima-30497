d542061d02963ce72dd3ea1909670f84b6213467
katsumi-lab <71427533+katsumi-lab@users.noreply.github.com>
2020-10-17 17:26:37 +0900

    バリデーションを設定する

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,5 +8,19 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
   
+  with_options presence: true do
+    validates :image
+    validates :name
+    validates :description
+    validates :category_id
+    validates :status_id
+    validates :shipping_cost_id
+    validates :prefecture_id
+    validates :shipping_timing_id
+    validates :price
+  end
 
+  validates  :price, numericality: 
+    {greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 } 
+  validates :price, format: { with: /\A[0-9]\w*\z/ }
 end
