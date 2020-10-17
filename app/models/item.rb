18399762077ecf53c79aacdac9f414adcf00aae1
katsumi-lab <71427533+katsumi-lab@users.noreply.github.com>
2020-10-17 21:27:48 +0900

    商品出品ページの実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,8 +3,8 @@ class Item < ApplicationRecord
   belongs_to_active_hash :category
   belongs_to_active_hash :status
   belongs_to_active_hash :shipping_cost
-  belongs_to_active_hash :shipping_timing
   belongs_to_active_hash :prefecture
+  belongs_to_active_hash :shipping_timing
   belongs_to :user
   has_one_attached :image
   
