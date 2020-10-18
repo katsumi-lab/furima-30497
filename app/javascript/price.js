a8bdad5b6ee2d434ad0e776f319ccf709b1b69a9
katsumi-lab <71427533+katsumi-lab@users.noreply.github.com>
2020-10-18 19:59:31 +0900

    JavaScriptの価格計算

--- app/javascript/price.js
+++ app/javascript/price.js
@@ -1,3 +1,13 @@
 window.addEventListener('load', () => {
-  console.log("OK");
+  const contents = document.getElementById("item-price");
+  const tax_field = document.getElementById("add-tax-price");
+  const profit_field = document.getElementById("profit");
+
+  contents.addEventListener("input",function(e){
+  let price = contents.value;
+  let tax = price * 0.1;
+  let profit = (price - tax);
+  tax_field.textContent = tax;
+  profit_field.textContent = profit;
+  })
 });
