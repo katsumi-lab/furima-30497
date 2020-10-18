a0ec0094f03c6fef5b6e3d4d212309e58cc6d2d4
katsumi-lab <71427533+katsumi-lab@users.noreply.github.com>
2020-10-18 11:37:29 +0900

    出品情報の登録の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,10 +7,15 @@ class ItemsController < ApplicationController
     @item = Item.new
   end
 
+  def create
+    @items = Item.new(item_params)
+    @items.valid?
+    @items.save
+  end
   private
 
-  def message_params
-    params.require(:items).permit(:image).merge(user_id: current_user.id)
+  def item_params
+    params.require(:item).permit(:name, :description, :category_id, :status_id, :shipping_cost_id, :prefecture_id, :shipping_timing_id, :price,:image).merge(user_id: current_user.id)
   end
 
 end
