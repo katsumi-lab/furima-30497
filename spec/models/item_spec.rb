617f395f4a68a525be2b57b193c08ca1aa9fcdb8
katsumi-lab <71427533+katsumi-lab@users.noreply.github.com>
2020-10-18 12:30:14 +0900

    fakerのインストール

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '商品登録ができる'
+  it "商品画像が空と登録できない" do
+  end
+  it "商品名が空と登録できない" do
+  end
+  it "商品カテゴリーが空と登録できない" do
+  end
+  it "商品の状態が空と登録できない" do
+  end
+  it "配送料の負担が空と登録できない" do
+  end
+  it "発送元の地域が空と登録できない" do
+  end
+  it "発送までの日数が空と登録できない" do
+  end
+  it "価格が空と登録できない" do
+  end
+  it "価格の範囲が¥300〜¥9,999,999の間でないと登録できない" do
+  end
+  it "価格は半角数字でないと登録できない" do
+  end
+  it "全ての項目が入力されていないと登録できない" do
+  end
 end
