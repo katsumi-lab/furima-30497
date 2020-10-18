05d12187e124e0ddf2e982fea0e7e1f47bbc8585
katsumi-lab <71427533+katsumi-lab@users.noreply.github.com>
2020-10-18 21:41:19 +0900

    テストコードの不要なダブルクォーテーション削除

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -20,31 +20,31 @@ RSpec.describe Item, type: :model do
     end
 
     it "商品カテゴリーが空と登録できない" do
-      @item.category_id = "1"
+      @item.category_id = 1
       @item.valid?
       expect(@item.errors.full_messages).to include("Category is not included in the list")
     end
 
     it "商品の状態が空と登録できない" do
-      @item.status_id = "1"
+      @item.status_id = 1
       @item.valid?
       expect(@item.errors.full_messages).to include("Status is not included in the list")
     end
 
     it "配送料の負担が空と登録できない" do
-      @item.shipping_cost_id = "1"
+      @item.shipping_cost_id = 1
       @item.valid?
       expect(@item.errors.full_messages).to include("Shipping cost is not included in the list")
     end
 
     it "発送元の地域が空と登録できない" do
-      @item.prefecture_id = "1"
+      @item.prefecture_id = 1
       @item.valid?
       expect(@item.errors.full_messages).to include("Prefecture is not included in the list")
     end
 
     it "発送までの日数が空と登録できない" do
-      @item.shipping_timing_id = "1"
+      @item.shipping_timing_id = 1
       @item.valid?
       expect(@item.errors.full_messages).to include("Shipping timing is not included in the list")
     end
