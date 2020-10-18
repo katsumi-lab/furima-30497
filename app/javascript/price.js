1c5fd02b9ba36bf12ae3c85ffa63e07a8461f82f
katsumi-lab <71427533+katsumi-lab@users.noreply.github.com>
2020-10-18 21:38:19 +0900

    indexアクション・コメントアウト・factories.items.rbの修正

--- app/javascript/price.js
+++ app/javascript/price.js
@@ -2,7 +2,7 @@ window.addEventListener('load', () => {
   const contents = document.getElementById("item-price");
   const tax_field = document.getElementById("add-tax-price");
   const profit_field = document.getElementById("profit");
-
+  
   contents.addEventListener("input",function(e){
   let price = contents.value;
   let tax = price * 0.1;
--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -4,10 +4,10 @@ FactoryBot.define do
       price              {"0000000"}
       description        { Faker::Lorem.paragraph }
       user_id            {"0"}
-      category_id        {"0"}
-      status_id          {"0"}
-      shipping_cost_id   {"0"}
-      shipping_timing_id {"0"}
-      prefecture_id      {"0"}
+      category_id        {2}
+      status_id          {2}
+      shipping_cost_id   {2}
+      shipping_timing_id {2}
+      prefecture_id      {2}
   end
 end
