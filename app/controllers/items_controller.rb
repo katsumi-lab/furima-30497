618b6088d32d02f92fbca963c74b5c29f4568b6d
katsumi-lab <71427533+katsumi-lab@users.noreply.github.com>
2020-10-19 20:03:03 +0900

    編集画面のボックスに元のデータの表示

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: :index
+  before_action :move_to_index, except: [:index, :show]
   
   def index
     @items = Item.all.order("created_at DESC")
@@ -24,11 +25,24 @@ class ItemsController < ApplicationController
   end
 
   def edit
+    @item = Item.find(params[:id])
   end
+
+  def update
+    item = Item.find(params[:id])
+    item.update(item_params)
+    
+  end
+
   private
 
   def item_params
     params.require(:item).permit(:name, :description, :category_id, :status_id, :shipping_cost_id, :prefecture_id, :shipping_timing_id, :price,:image).merge(user_id: current_user.id)
   end
 
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
+  end
 end
