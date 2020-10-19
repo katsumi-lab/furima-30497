3bec7adf5391225ed22c11effb66f44d7ddf783c
katsumi-lab <71427533+katsumi-lab@users.noreply.github.com>
2020-10-19 10:29:23 +0900

    authenticate_user!をitems_controllerに記述、factoryの不要な記述の削除

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,9 +1,8 @@
 FactoryBot.define do
   factory :item do
       name               {"name"}
-      price              {"300-9999999"}
+      price              {300}
       description        { Faker::Lorem.paragraph }
-      user_id            {"0"}
       category_id        {2}
       status_id          {2}
       shipping_cost_id   {2}
