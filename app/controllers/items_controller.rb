404f610020be327cbaf9e88b58dd165ee8644757
katsumi-lab <71427533+katsumi-lab@users.noreply.github.com>
2020-10-20 16:56:57 +0900

    直接アクセスできない条件分岐

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,6 +2,7 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :move_to_index, except: [:index, :show]
   before_action :set_item, only:[:show, :edit, :update, :destroy]
+  before_action :redirect_to_index, only: :destroy
 
   def index
     @items = Item.all.order('created_at DESC')
@@ -46,10 +47,18 @@ class ItemsController < ApplicationController
   end
 
   def move_to_index
-    redirect_to action: :index unless user_signed_in?
+    unless user_signed_in?
+      redirect_to action: :index 
+    end
   end
 
   def set_item
     @item = Item.find(params[:id])
+  end
+
+  def redirect_to_index
+    unless user_signed_in? == @item.user.id
+      redirect_to action: :index 
+    end
   end
 end
