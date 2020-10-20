69b80eda7dee029886cd802a0780f2574fa1f5e0
katsumi-lab <71427533+katsumi-lab@users.noreply.github.com>
2020-10-20 16:14:11 +0900

    destroyメソッドの集約

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :move_to_index, except: [:index, :show]
-  before_action :set_item, only:[:show, :edit, :update]
+  before_action :set_item, only:[:show, :edit, :update, :destroy]
 
   def index
     @items = Item.all.order('created_at DESC')
@@ -37,9 +37,9 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    item = Item.find(params[:id])
-    if item.destroy
-      redirect_to item_path(item.id)
+    
+    if @item.destroy
+      redirect_to root_path
     else
       render edit_item_path
     end
