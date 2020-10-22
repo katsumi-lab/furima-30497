bc84d52fa3f3bba1c0d38fd5fac32097d765510f
katsumi-lab <71427533+katsumi-lab@users.noreply.github.com>
2020-10-22 21:52:49 +0900

    購入ページへの直接アクセスできない記述

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,6 @@
 class OrdersController < ApplicationController
-before_action :set_item, only:[:index, :create]
+  before_action :move_to_index, except: :index
+  before_action :set_item, only:[:index, :create]
 
   def index
     @order_delivery = OrderDelivery.new
@@ -17,6 +18,12 @@ before_action :set_item, only:[:index, :create]
   end
 
   private
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
+  end
 
   def set_item
     @item = Item.find(params[:item_id])
