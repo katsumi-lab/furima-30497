4257f9887d39a3639f84ade3c7239a1f71264537
katsumi-lab <71427533+katsumi-lab@users.noreply.github.com>
2020-10-22 21:27:36 +0900

    サーバーサイド環境変数設定

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,5 +1,5 @@
 const pay = () => {
-  Payjp.setPublicKey("pk_test_00fdf39c3ce2adc6ccc9021d");
+  Payjp.setPublicKey(process.env.PAYJP_PUBLIC_KEY);
   const form = document.getElementById("charge-form");
   form.addEventListener("submit", (e) => {
     e.preventDefault();
