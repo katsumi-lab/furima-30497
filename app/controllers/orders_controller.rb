db0aea0a85fcebffbb08eb88facbede4054d0e56
katsumi-lab <71427533+katsumi-lab@users.noreply.github.com>
2020-10-23 17:09:39 +0900

    javascriptの読み込みエラー修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,5 @@
 class OrdersController < ApplicationController
-  before_action :move_to_index, except: :index
+  before_action :move_to_index, except: [:index, :create]
   before_action :set_item, only:[:index, :create]
 
   def index
