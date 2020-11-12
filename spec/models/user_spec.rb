dfd3b2ebc1f4d8d0ef6b8cc4efe8695295512731
katsumi-lab <71427533+katsumi-lab@users.noreply.github.com>
2020-11-12 10:37:11 +0900

    単体テストコードの一部修正

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -11,11 +11,6 @@ RSpec.describe User, type: :model do
       expect(@user.errors.full_messages).to include("Nickname can't be blank")
     end
 
-    it 'emailが空では登録できない' do
-      @user.email = ''
-      @user.valid?
-      expect(@user.errors.full_messages).to include("Email can't be blank")
-    end
     it '重複したemailが存在する場合登録できない' do
       @user.save
       another_user = FactoryBot.build(:user)
